399c70d571c61a5e4399944e3cf2299263417391
kanishkajain2005-byte <kanishkajain2005@gmail.com>
2025-10-09 16:52:39 +0530

    Add files via upload

--- Diabetespredictionapp.py
+++ Diabetespredictionapp.py
@@ -11,14 +11,16 @@ import pickle
 import streamlit as st
 import pandas as pd
 
-loaded_model = pickle.load(open('/Users/Jaishreenirmala/Desktop/Diabetes prediction/trained_model.sav', 'rb'))
-loaded_scaler = pickle.load(open('/Users/Jaishreenirmala/Desktop/Diabetes prediction/scaler.pkl', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 def diabetes_prediction(input_data):
-    # taking the input for the prediction.
     input_data = pd.DataFrame([input_data], columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction'])
-    # scaling the taken input.
-    scaled_input_data = loaded_scaler.transform(input_data)
+    # now scaling the input taken.
+    mean = input_data.mean()
+    std_dev = input_data.std()
+    return (input_data - mean) / std_dev
+    scaled_input_data = loaded_scaler.fit_transform(input_data)
     # now converting the input into a array.
     scaled_input_data_as_numpy_array = np.asarray(scaled_input_data)
     # reshaping the array.
@@ -28,46 +30,52 @@ def diabetes_prediction(input_data):
     # putting the condition for prediction.
     print(prediction)
     if (prediction[0] == 0):
-      return ('The person is not diabetic')
+         return ('The person is not diabetic')
+       
     else:
-      return ('The person is diabetic')
-  
+         return ('The person is diabetic')
+
+
 def main():
-    # creating the title of the app.
     st.title('Diabetes Prediction app')
     
-    # Taking the inputs of the following values.
-    
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose value')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin value')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    
-    diognosis = ''
-    
-    if st.button('Diabetes test result'):
+    # Use st.form to group the inputs and the button
+    with st.form(key='diabetes_form'):
+        Pregnancies = st.number_input('Number of Pregnancies', value=0.0)
+        Glucose = st.number_input('Glucose value', value=0.0)
+        BloodPressure = st.number_input('Blood Pressure value', value=0.0)
+        SkinThickness = st.number_input('Skin Thickness value', value=0.0)
+        Insulin = st.number_input('Insulin value', value=0.0)
+        BMI = st.number_input('BMI value', value=0.0)
+        DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value', value=0.0)
         
+        # Using submit button for the result.
+        submit_button = st.form_submit_button(label='Diabetes test result')
         
+    # The code below this will only be executed after the form is submitted
+    if submit_button:
         try:
-            
             input_list = [
-                float(BMI), float(Glucose), float(SkinThickness), 
-                float(Insulin), float(BloodPressure), 
-                float(Pregnancies), float(DiabetesPedigreeFunction)
+                float(Pregnancies), float(Glucose), float(BloodPressure),
+                float(SkinThickness), float(Insulin), float(BMI),
+                float(DiabetesPedigreeFunction)
             ]
             
-            #
-            diagnosis = diabetes_prediction(input_list)
+            # Check for invalid values like 0.0 or others if necessary
             
+            if all(value == 0.0 for value in input_list):
+                 st.error("Please fill in all the input fields with valid numbers.")
+            else:
+                 diagnosis = diabetes_prediction(input_list)
+                 st.success(diagnosis)
+        
+        except ValueError:
+            # This block will handle issues if input values aren't numbers
+            st.error("Please ensure all inputs are valid numbers.")
+
             st.success(diagnosis)
-            # putting the condition for empty entries.
-        except ValueError:  
-            
-            st.error("Please fill in all the input fields with valid numbers.")
+        
+        
 
-
-if __name__ == '__main__'  :
+if __name__ == '__main__':
     main()
